fc9ea4312a46e88032420702b54fb9b4cc7f856d
Jiang-Feiyu <msjfy223@connect.hku.hk>
2025-04-14 13:05:59 +0800

    update: app_main.c

--- MQTT_Test/publish1.py
+++ MQTT_Test/publish1.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 from random import randrange, uniform
 import time
 
-mqttBroker = "localhost"
+mqttBroker = "mqtt.eclipseprojects.io"
 client = mqtt.Client("Temperature_Inside")
 client.connect(mqttBroker)
 
--- MQTT_Test/subscribe.py
+++ MQTT_Test/subscribe.py
@@ -4,7 +4,7 @@ import time
 def on_message(client, userdata, message):
     print("Received message: ", str(message.payload.decode("utf-8")))
 
-mqttBroker = "localhost"
+mqttBroker = "192.168.43.16"
 client = mqtt.Client("Smartphone")
 client.connect(mqttBroker)
 
